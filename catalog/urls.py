db5530bc4bc2677bd515363e013c50cf629ef118
neuryan <neuryan@qq.com>
2018-10-16 14:37:57 +0800

    python version local library
    python project: local library

---
+++ catalog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+]
+
+urlpatterns += [
+    path('', views.index, name='index'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    #re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'), # this uses RE
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
